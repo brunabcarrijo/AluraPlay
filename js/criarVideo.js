c70540c60979d3c58fd461b7930ed9d724e50d5a
Bruna Carrijo <brunabcarrijoo@gmail.com>
2024-07-26 17:14:39 -0300

    Novo envio das atualizações do projeto

--- js/criarVideo.js
+++ js/criarVideo.js
@@ -1,7 +1,12 @@
 const formulario = document.querySelector("[data-formulario]");
 
-function criaVideo(){
-    const imagem = document.querySelector("[data-imagem]");
-    const url = document.querySelector("[data-url]");
-    const titulo = document.querySelector("[data-titulo]");
+function criaVideo(evento){
+    evento.preventDefault();
+    
+    const imagem = document.querySelector("[data-imagem]").value;
+    const url = document.querySelector("[data-url]").value;
+    const titulo = document.querySelector("[data-titulo]").value;
+    const descricao = Math.floor(Math.random() * 10).toString();
 }
+
+formulario.addEventListener("submit", evento => criarVideo(evento));
