febd120d913f3215fc41002bfeaae8c1c116f14e
Bruna Carrijo <brunabcarrijoo@gmail.com>
2024-07-29 18:32:25 -0300

    Novo envio das atualizações do projeto

---
+++ js/buscarVideo.js
@@ -0,0 +1,25 @@
+import { conectaApi } from "./conectaApi.js";
+import constroiCard from "./mostrarVideos.js";
+
+async function buscarVideo(evento){
+    evento.preventDefault();
+    const dadosDePesquisa = document.querySelector("[data-pesquisa]").value.toLowerCase();
+    const busca = await conectaApi.buscaVideo();
+
+    const lista = document.querySelector("[data-lista]");
+
+    while(lista.firstChild){
+        lista.removeChild(lista.firstChild);
+    }
+
+    busca.forEach(elemento => {
+        if (elemento.titulo.toLowerCase().includes(dadosDePesquisa)){
+            lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem))
+        }
+})
+    
+}
+
+const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]");
+
+botaoDePesquisa.addEventListener("click", evento => buscarVideo(evento))
