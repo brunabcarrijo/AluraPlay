93252717abddfccb6a044801ca5201597c2b4387
Bruna Carrijo <brunabcarrijoo@gmail.com>
2024-07-30 17:02:15 -0300

    último envio das atualizações do projeto

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -7,6 +7,7 @@ async function buscarVideo(evento){
     const busca = await conectaApi.buscaVideo();
 
     const lista = document.querySelector("[data-lista]");
+    let encontrouVideos = false;
 
     while(lista.firstChild){
         lista.removeChild(lista.firstChild);
@@ -15,9 +16,13 @@ async function buscarVideo(evento){
     busca.forEach(elemento => {
         if (elemento.titulo.toLowerCase().includes(dadosDePesquisa)){
             lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem))
+            encontrouVideos = true;
         }
 })
-    
+
+    if (!encontrouVideos) {
+        lista.innerHTML = `<h2 class="mensagem__titulo">Não existem vídeos com esse termo</h2>`
+    }
 }
 
 const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]");
